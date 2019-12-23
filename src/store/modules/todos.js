c88ed5afbbdfb40b532a3483d937248531fb727f
sharbel93 <sharbel@techfactory.ltd>
2019-12-24 00:36:17 +0300

    set data using vuex mutations

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -13,10 +13,14 @@ const getters = {
 const actions = {
     async fetchTodos({ commit }) {
         const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
-         console.log(response.data);
+         // console.log(response.data);
+        commit('setTodos', response.data);
     }
 };
-const mutations = {};
+
+const mutations = {
+    setTodos: (state, todos) => (state.todos = todos)
+};
 
 export default {
     state,
