ac3feb6e02b7f1f2406118e7fb32fb5cd6610523
sharbel93 <sharbel@techfactory.ltd>
2019-12-24 00:18:00 +0300

    fetch data using mapActions

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,16 +1,7 @@
 import axios from 'axios';
 
 const state = {
-    todos: [
-        {
-            id: 1,
-            title: 'Todo One'
-        },
-        {
-            id: 2,
-            title: 'Todo Two'
-        }
-    ]
+    todos: []
 
 };
 
@@ -19,7 +10,12 @@ const getters = {
 };
 
 
-const actions = {};
+const actions = {
+    async fetchTodos({ commit }) {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+         console.log(response.data);
+    }
+};
 const mutations = {};
 
 export default {
