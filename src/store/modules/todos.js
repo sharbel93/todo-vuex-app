b936521791d540737136ca5266dca7dd93da0953
sharbel93 <sharbel@techfactory.ltd>
2019-12-24 12:51:36 +0300

    ability to filter todos

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -24,6 +24,14 @@ const actions = {
     async deleteTodo({ commit }, id) {
         await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`); // remove from back-end/ server
         commit('removeTodo', id);
+    },
+
+    async filterTodos({ commit }, e) {
+        // Get selected number
+        const limit  = parseInt(e.target.options[e.target.options.selectedIndex].innerText);
+        const response = await axios.get(
+            `https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
+        commit('setTodos', response.data);
     }
 };
 
